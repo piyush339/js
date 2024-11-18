618ebf6208b9a25bdd2c642dbba5cedae74e281c
Piyush Bamniya <77956411+piyush339@users.noreply.github.com>
2024-11-18 17:16:33 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+ const accountId = 144553
+ let accountEmail = "piyushbamniya3@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "jaipur"
+
+ //  accountId = 2
+console.log(accountId);
+accountEmail = "fc@fscs"
+accountPassword = "554333"
+accountCity = "bengluru"
+let accountState; 
+
+/*
+prefer not use to var prefer let
+*/
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newwer virsion
+
+alert(3+3)
+console.log(3+3); 
+console.log("piyusu") //code readblity that should be high
+
+
+// use mdn 
+
+//but this is a base concept ECMAScript
+
+// data types-->
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; //undefind
+
+
+//number -> 2 to pow 53
+//bigint
+//string -> "   "
+//boollean -> true/false
+
+//null -> standalone value
+
+//undefine -> 
+//symbole -> unique
+
+//object 
+
+console.log(typeof age); //but we cant use age == null because null is a object
+ // undefine != null;
+
