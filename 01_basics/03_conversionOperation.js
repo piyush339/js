1a409acf8ec7bd319e024f11843ee0837759f1db
Piyush Bamniya <77956411+piyush339@users.noreply.github.com>
2024-11-20 12:19:37 +0000

    conversion of

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//find vatiable type
+//
+
+let score = null
+let B = 234
+let A = undefined
+let con = Number(score)
+
+console.log(typeof con)
+console.log(con);/// NaN 
+
+// if we can change any number to number easyly convert
+
+//"22" -. 33
+//"232de" -> nan
+// true -> 1 ;
+//false -> 0
+
+let islogedin = 1
+
+let boolean = Boolean(islogedin)
+console.log(boolean);
+
+//1 convert boolean type -> true
+//0 convert boolean type -> false
